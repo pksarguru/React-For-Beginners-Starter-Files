ed52cfbff646f3da7f4b97063b09ff20ed54bc41
Pavan Sarguru <pavan.sarguru@gamut.com>
2018-03-02 13:41:54 -0600

    finish vid 10

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Inventory extends React.Component {
+  render() {
+    return (
+      <header className="top">
+        <h1>
+          Inventory!!
+        </h1>
+      </header>
+    )
+  }
+}
+
+export default Inventory;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    return (
+      <header className="top">
+        <h1>
+          Order!!!
+        </h1>
+      </header>
+    )
+  }
+}
+
+export default Order;
